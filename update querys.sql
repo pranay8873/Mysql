7bd606f76230c79e57f88470be63b7ca28069799
pranay8873 <pranaypuvaati@gmail.com>
2026-03-29 20:07:23 +0530

    updating and deleting querys

---
+++ update querys.sql
@@ -0,0 +1,5 @@
+USE startersql;
+-- SELECT * FROM users;
+-- UPDATE users SET salary=60000,email='nekuyenduku6@gmail.com' WHERE id=1;
+-- UPDATE users SET salary=salary+10000 WHERE id>0 AND salary<60000;
+SELECT * FROM users;
